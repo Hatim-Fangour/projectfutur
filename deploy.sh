e4ec24e54af419ac063c57e82f2357aef03ad667
Hatim-Fangour <hatimfangour87@gmail.com>
2026-02-25 21:09:22 +0000

    working on February 25, 2026 09:09 PM

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 
 echo "Pushing the repo ..." 
 current_datetime=$(date "+%B %d, %Y %I:%M %p")
-git checkout -b postop
 git add .
 git commit -m "working on $current_datetime"
 git push -u origin postop
